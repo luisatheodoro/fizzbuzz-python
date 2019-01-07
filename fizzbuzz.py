d517d04501099ba33407a2e12601857241104aef
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-07 23:46:49 +0000

    adding firt unittest for multiples of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def FizzBuzz(n):
-  if n == 3:
+  if n == 15:
+      return 'FizzBuzz'
+  elif n == 3:
       return 'Fizz'
   elif n == 5:
       return 'Buzz'
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,6 +8,9 @@ class FizzBuzzTest(unittest.TestCase):
     def test_return_buzz_with_multiples_of_5(self):
         self.assertEqual('Buzz', fizzbuzz.FizzBuzz(5))
         
+    def test_return_fizzbuzz_with_multiples_of_15(self):
+        self.assertEqual('FizzBuzz', fizzbuzz.FizzBuzz(15))
+        
         
         
 if __name__ == "__main__":
